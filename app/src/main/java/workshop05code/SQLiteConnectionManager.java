ffbd552c69ade143881d3667a11874612d154ab8
yagirl-belle <zahlia.hamer@students.mq.edu.au>
2024-03-21 17:22:06 +1100

    Fixed 5.2.3 to be correct

--- app/src/main/java/workshop05code/SQLiteConnectionManager.java
+++ app/src/main/java/workshop05code/SQLiteConnectionManager.java
@@ -132,6 +132,8 @@ public class SQLiteConnectionManager {
 
         try (Connection conn = DriverManager.getConnection(databaseURL);
                 PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setInt(1, id);
+            pstmt.setString(2, word);
             pstmt.executeUpdate();
         } catch (SQLException e) {
             System.out.println(e.getMessage());
